d034cb926e44182dc7b913db20e4dfda556aeb4b
irumvag <irumvagadanaclet@gmai.com>
2025-11-09 23:15:57 +0200

    fix:hero content margin top on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  server:{
-    port:3010,
-  },
+import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+
+  return {
+    plugins: [
+      tailwindcss(),
+      ...(isBuild
+        ? [viteStaticCopy({
+              targets: [
+                {
+                  src: 'src/**/*',
+                  dest: 'src/', 
+                },
+              ],
+            }),
+          ]
+        : []),
+    ],
+    server: {
+      port: 3010,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          contact: resolve(__dirname, 'contacts-page.html'),
+          privacy: resolve(__dirname, 'dataprivacy.html'),
+        },
+      },
+    },
+  }
 })
